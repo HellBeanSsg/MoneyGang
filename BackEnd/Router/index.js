766e5a47291c5eb7e2371b5921d9c2d0d10877e5
Bin <jo7hb@naver.com>
2021-01-27 15:14:04 +0900

    DB_connection

--- BackEnd/Router/index.js
+++ BackEnd/Router/index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const express       = require("express");
+const router        = express.Router();
 
 /* DB_Connection */
-const DB_Connect    = require("../DB/DB_connect");
-DB_Connect();
+const dbconnect     = require("../DB/DB_connect");
+dbconnect();
 
 /* DB_Schema */
-const Student_Sch   = require("../DB/DB_TestSchema.js");
-const hellbeanssg   = Student_Sch();
+const studentsch    = require("../DB/DB_TestSchema.js");
+const hellbeanssg   = studentsch();
 
 router.get("/", (req, res) => {
     res.send("Hello World!");
