513a7ccfae18357566aaba478b109f31a71fca83
jsy0404 <37055726+jsy0404@users.noreply.github.com>
2021-03-04 23:59:03 +0900

    hot-fix : Remove conflict (#25)
    * feat : Add functions with bitmex driver.
    
    There are four functions.
    - `Order` function is always `post-only` command.
      - `Buy` command always commit order with `0.5` reduced price.
      - `Sell` command always commit order with `0.5` increased price.
    - `deleteOrder` function remove all orders not commited.
    - `getPosition` function return position what you have.
    - `getOrder` function return all orders not commited.
    - `sendRequest` should not be called by outside(must be private).
    - these functions are wrote in HTTP rest api.
    
    * feat : Add callback driver
    
    There are two callback methods.
    - `order` should be called by algorithm.
      - `Long` position order should be there are no other position.
      - `Short` position order should be there are
    - `tradeInfo` is called by `BitmexDriver` with trade tracing.
    
    * feat : Add OrderBookDriver.
    
    This driver should be called by `CallbackDriver` via `BitmexDriver`.
    
    There are 2 functions.
    - `orderBookInfo` function should be called like callback function.
      - This should be called when orderbook is changed.
    - `addQuote` should be called by `orderBookInfo`(private).
      - This method tracing all users' order and catch the specific order.
        - Specific order can be same order size ordered as twice in same size.
        - Also can be very large order size.
    - `resetQuoteBook` also should be only called by `addQuote`.
      - This is called when QuoteBook is large enough to manage.
      - `Large` threshold can be anything, default is `40`.
    - `sortDictByKey` is for sort and print quoteBook.
    
    * refactor : Add typing in ts
    
    * feat : Add TestDriver.
    
    TestDriver is for simulate trading with deposit.
    
    - It has no slippage.
    - Just use only `Buy` or `Sell`
    - Default fee is 0.05% in Bidirectional.
      - Fee is taken with `Buy` side price.
      - Fee is taken at `Sell` time.
    
    * refactor : Add tsconfig file
    
    * refactor : Add package.json
    
    * refactor : codacy check
    
    * refactor: Modify complexity with CallbackDriver::order
    
    * feat : Add algorithm
    
    * fix : check codacy error
    
    * fix : remove js
    
    * fix : check codacy error
    
    * feat : Modulize algorithm
    
    * fix : Remove test files
    
    * fix : Remove .js files
    
    * fix : Remove typing.d.ts in driver directory
    
    * fix : Remove conflict
    
    * fix : Remove *.js
    
    * fix : Remove conflict
    
    * fix : Replace snake case to camel case
    
    * fix : Remvoe complexity with driver/OrderBook.ts

--- algorithm/GoldenCross.ts
+++ algorithm/GoldenCross.ts
@@ -1,5 +1,4 @@
 import { MovingAverage } from "algorithm/MovingAverage";
-<<<<<<< HEAD
 import { Algorithm } from "algorithm/AlgorithmModule";
 
 export
@@ -7,10 +6,10 @@ class GoldenCross extends Algorithm {
 	longMovingAvg:	MovingAverage;
 	shortMovingAvg: MovingAverage;
 
-	constructor(short_length: number, long_length: number, shortBaseTime: number, longBaseTime: number, receiver: Function) {
+	constructor(shortLength: number, longLength: number, shortBaseTime: number, longBaseTime: number, receiver: Function) {
 		super(receiver);
-		this.shortMovingAvg = new MovingAverage(short_length, shortBaseTime, this.shortMvAvgChecker);
-		this.longMovingAvg  = new MovingAverage(long_length, longBaseTime, this.longMvAvgChecker);
+		this.shortMovingAvg = new MovingAverage(shortLength, shortBaseTime, this.shortMvAvgChecker);
+		this.longMovingAvg  = new MovingAverage(longLength, longBaseTime, this.longMvAvgChecker);
 	}
 
 	setLastPrice(price: number): void {
@@ -24,28 +23,6 @@ class GoldenCross extends Algorithm {
 	}
 
 	longMvAvgChecker(mvAvg: number): void {
-=======
-
-export
-class GoldenCross {
-	longMovingAvg:	MovingAverage;
-	shortMovingAvg: MovingAverage;
-
-	constructor(short_length: number, long_length: number, shortBaseTime: number, longBaseTime: number, sendSignal: Function) {
-		this.shortMovingAvg = new MovingAverage(short_length);
-		this.longMovingAvg  = new MovingAverage(long_length);
-
-		setInterval(() => {
-
-		}, shortBaseTime*1000);
-
-		setInterval(() => {
-
-		}, longBaseTime*1000);
-	}
-
-	async pushPeriodic() {
->>>>>>> 533e12b3112a603a68ea71317e12ba5b52c28c38
 
 	}
 }
